b6eb692ff5bc685174b80b5c43d88d6189787bba
Theogene Hashimwimana <hashimwimanatheogene34@gmail.com>
2025-05-14 11:24:48 +0200

    vite

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,16 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      'lib': path.resolve(__dirname, 'src/lib') // Add this alias for the `lib` folder
+      'lib': path.resolve(__dirname, 'src/lib'),
     },
+  },
+  build: {
+    outDir: 'dist', // Vercel expects this as the output
   },
 });
